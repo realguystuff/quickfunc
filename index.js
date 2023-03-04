9969a63e6571e21f6033404e3be2638bb5f4bcd6
realguystuff <realguybackup@gmail.com>
2023-03-04 15:53:10 +0800

    error fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
   randomArray: require('./functions/randomArray.js'),
   oddEven: require('./functions/oddEven.js'),
   prime: require('./functions/prime.js'),
-  clean: require('./functions/clean.js'),
   averageArray: require('./functions/averageArray.js'),
   reverseArray: require('./functions/reverseArray.js'),
   sortArray: require('./functions/sortArray.js'),
